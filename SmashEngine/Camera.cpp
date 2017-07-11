d5ddc426f8dce4c82eea81364170b42eede570e7
Anshul Soni <soni.anshul93@gmail.com>
2017-07-10 23:56:11 -0700

    removing unused variable from camera

--- SmashEngine/Camera.cpp
+++ SmashEngine/Camera.cpp
@@ -23,14 +23,11 @@ namespace SmashEngine
 		fov(45.0f),
 		projectionMatrix(glm::mat4(1)),
 		viewMatrix(glm::mat4(1)),
-		position(glm::vec3(0, 0, 5)),
 		cameraSpeed(5.0f),
 		deltaTime(0),
 		right(glm::vec3(1)),
 		up(glm::vec3(1)),
-		direction(glm::vec3(1)),
-		horizontalAngle(3.14f),
-		verticalAngle(0.0f)
+		direction(glm::vec3(1))
 	{
 	}
 
